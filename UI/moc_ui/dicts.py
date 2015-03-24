96554cfe2648865df289b0aadf5e2b199c866dcc
AlexDWong <adwong16@bu.edu>
2015-03-24 19:20:37 -0400

    Update market dictionary

--- UI/moc_ui/dicts.py
+++ UI/moc_ui/dicts.py
@@ -15,7 +15,7 @@ reg_modal = {'id': 'createUser', 'action': '/register', 'method': 'post', 'title
 
 
 # CLOUDS PAGE
-test_vm_list_1 = [{'name': 'hadoop master', 'state': '=)', 'provider': 'HU-prod', 'image': 'centOS 7'},
+test_vm_list_1 = [{'name': 'hadoop master', 'state': '=)', 'provider': 'HU-prod', 'image': 'centOS 7.jpg'},
                 {'name': 'hadoop slave 1', 'state': '=)', 'provider': 'BU-prod', 'image': 'centOS 7'},
                 {'name': 'hadoop slave 2', 'state': '=)', 'provider': 'NE-prod', 'image': 'centOS 7'},
                 {'name': 'hadoop slave 2', 'state': 'build', 'provider': 'UMASS-prod', 'image': 'centOS 7'},
@@ -35,5 +35,11 @@ test_service_list = [{'name': 'Hadoop', 'state': '=)', 'provider': 'HU-prod', 'i
 test_market_list = [{'name': 'Service','market_choice_list': test_service_list}, 
                      {'name': 'Hive and Pig'}]
 
-
-
+test_market_list = [{'name':'Dell','provider':'Dell','description':'Dell Compute', 'version':'3.1.4', 'availability':'Yes', 'type':'Compute', 'image':'centOS 7'},
+                    {'name':'HP','provider':'HP','description':'HP Compute', 'version':'3.1.4', 'availability':'Yes', 'type':'Compute', 'image':'centOS 7'},
+                    {'name':'Intel','provider':'Intel','description':'Intel Compute', 'version':'3.1.4', 'availability':'Yes', 'type':'Compute', 'image':'centOS 7'},
+                    {'name':'EMC','provider':'EMC','description':'EMC Storage', 'version':'3.1.4', 'availability':'Yes', 'type':'Storage', 'image':'centOS 7'},
+                    {'name':'NetApp','provider':'NetApp','description':'NetApp Storage', 'version':'3.1.4', 'availability':'Yes', 'type':'Storage', 'image':'centOS 7'},
+                    {'name':'Brocade','provider':'Brocade','description':'Brocade Networking', 'version':'3.1.4', 'availability':'Yes', 'type':'Network', 'image':'centOS 7'},
+                    {'name':'Juniper','provider':'Juniper','description':'Juniper Networking', 'version':'3.1.4', 'availability':'Yes', 'type':'Network', 'image':'centOS 7'},
+                    {'name':'Cisco','provider':'Cisco','description':'Cisco Networking', 'version':'3.1.4', 'availability':'Yes', 'type':'Network', 'image':'centOS 7'}]
