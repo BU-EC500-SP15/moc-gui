e6be37b3a0ac8a9e74636ace12a1f8f16a14de3d
xuhang57 <xuh@bu.edu>
2015-03-29 15:05:31 -0400

    merge from CCI

--- UI/moc_ui/forms.py
+++ UI/moc_ui/forms.py
@@ -1,31 +1,67 @@
 from django import forms
 
+import models
+
 class login(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 class userRegister(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(initial='Username')
+    password = forms.CharField(initial='Password', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(initial='Password', widget=forms.PasswordInput)
 
+# project actions
 class createProject(forms.Form):
-    name = forms.CharField()
-    action = forms.CharField(widget=forms.HiddenInput(), initial='create')
+    name = forms.CharField(initial='Project Name')
 
 class deleteProject(forms.Form):
-    name = forms.CharField()
-    action = forms.CharField(widget=forms.HiddenInput(), initial='delete')
+    name = forms.CharField(initial='Project Name')
 
-class createVM(forms.Form):
-    providers = ['BU-PROD', 'HU-PROD', 'NE-PROD', 'UMASS-PROD', 'MIT-PROD']
+# cluster actions
+class createClusterAccount(forms.Form):
+    clusters = [('buprod', 'BU-PROD'), ('huprod', 'HU-PROD'), ('neprod', 'NE-PROD'), 
+                 ('umassprod', 'UMASS-PROD'), ('mitprod', 'MIT-PROD')]
+    cluster = forms.ChoiceField(widget=forms.Select, choices=clusters)
+    cluster_username = forms.CharField()
+    cluster_password = forms.CharField(widget=forms.PasswordInput)
+
+class deleteClusterAccount(forms.Form):
+    cluster_username = forms.CharField(initial='Cluster Username')
+
+# cluster Project actions
+class createOSProject(forms.Form):
+    clusterUsernames = []
+    for p in models.ClusterAccount.objects.all().values('cluster_username').distinct():
+        clusterUsernames.append((p['cluster_username'], p['cluster_username']))
+
+    cluster = forms.ChoiceField(widget=forms.Select, choices=clusterUsernames)
+    project_name = forms.CharField()
+    user_name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class deleteOSProject(forms.Form):
     name = forms.CharField()
-    provider= forms.ChoiceField(widget=forms.RadioSelect, choices=providers)
-    action = forms.CharField(widget=forms.HiddenInput(), initial='create')
+
+# vm actions
+class createVM(forms.Form):
+    name        = forms.CharField()
+
+    osProjects = []
+    for p in models.OSProject.objects.all().values('name').distinct():
+        osProjects.append((p['name'], p['name']))
+
+    OSProject = forms.ChoiceField(widget=forms.Select, choices=osProjects)
+
+    projects    = []
+    for p in models.UIProject.objects.all().values('name').distinct():
+        projects.append((p['name'], p['name']))
+
+    project     = forms.ChoiceField(widget=forms.Select, choices=projects)
 
 class deleteVM(forms.Form):
     name = forms.CharField()
-    action = forms.CharField(widget=forms.HiddenInput(), initial='delete')
 
 class controlVM(forms.Form):
+    name = forms.CharField()
     action = forms.CharField()
